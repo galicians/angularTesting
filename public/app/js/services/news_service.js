4db740e1ba39de64ab4a0e87416be8d3f3d48183
galicians <galicians@gmail.com>
2015-01-01 13:01:53 +0000

    news service being tested

---
+++ public/app/js/services/news_service.js
@@ -0,0 +1,20 @@
+
+
+var newsApp = angular.module('newsApp',['ngResource','newsAppServices']);
+
+var newsAppServices = angular.module('newsAppServices',[])
+
+newsAppServices.factory('newsService', ['$resource','$q', function($resource,$q) {
+    return {
+        getnews: function(){
+            var deferred = $q.defer();
+            $resource('http://localhost:3000/allnews').query()
+            .$promise.catch(function(error){
+                deferred.reject(error);
+            });
+              
+            return deferred.promise;
+        }
+    }
+}])
+
---
+++ test/news_service_spec.js
@@ -0,0 +1,45 @@
+
+describe('News Service', function() {
+    var $httpBackend;
+    var service;
+    var newsUrl = 'http://localhost:3000/allnews';
+
+    beforeEach(module('newsApp'));
+    beforeEach(module('newsAppServices'));
+
+    beforeEach(inject(function(_$httpBackend_, $injector) {
+
+        createNewsService = function() {
+            return $injector.get('newsService')
+        }
+
+        $httpBackend = _$httpBackend_;
+        service = createNewsService();
+    }));
+    
+    describe("When getting all news", function() {
+        it("Should make a call to the API", function() {
+            $httpBackend.expectGET(newsUrl).respond(200)
+            service.getnews();
+            $httpBackend.verifyNoOutstandingExpectation();
+        });
+        it("Should send an error when API fails", function() {
+            $httpBackend.whenGET(newsUrl).respond(500)
+
+            var err;
+
+            service.getnews().catch(function(e) {
+                err = e;
+            })
+
+            $httpBackend.flush();
+
+            expect(err).toBeDefined();
+        });
+        it("should send data when API is successful", function() {
+            $httpBackend.whenGET(newsUrl).respond(200, [{name: 'test'}]);
+
+            var data;
+        });
+    });
+});
